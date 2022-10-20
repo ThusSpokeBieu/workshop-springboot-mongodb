b1f5c6d87bdf076483b46036f5a726a880be29ec
Gabriel Messias Costa <gmessiasc@gmail.com>
2022-10-20 17:17:25 -0300

    connecting mongodb with repository and service

---
+++ src/main/java/com/gmess/workshopmongodb/services/UserService.java
@@ -0,0 +1,19 @@
+package com.gmess.workshopmongodb.services;
+
+import com.gmess.workshopmongodb.domain.User;
+import com.gmess.workshopmongodb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll(){
+        return repo.findAll();
+    }
+}
