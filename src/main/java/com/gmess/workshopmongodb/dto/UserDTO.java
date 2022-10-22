021b654309c80e438deb78e29f61b2204e5ac955
Gabriel Messias Costa <gmessiasc@gmail.com>
2022-10-22 10:11:41 -0300

    adding lombok

--- src/main/java/com/gmess/workshopmongodb/dto/UserDTO.java
+++ src/main/java/com/gmess/workshopmongodb/dto/UserDTO.java
@@ -1,10 +1,16 @@
 package com.gmess.workshopmongodb.dto;
 
 import com.gmess.workshopmongodb.domain.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serial;
 import java.io.Serializable;
 
+@NoArgsConstructor
+@Getter
+@Setter
 public class UserDTO implements Serializable {
     @Serial
     private static final long serialVersionUID = 1909797553118131099L;
@@ -12,36 +18,10 @@ public class UserDTO implements Serializable {
     private String name;
     private String email;
 
-    public UserDTO(){
-    }
-
     public UserDTO(User obj){
         id = obj.getId();
         name = obj.getName();
         email = obj.getEmail();
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
--- src/main/java/com/gmess/workshopmongodb/resources/exception/StandardError.java
+++ src/main/java/com/gmess/workshopmongodb/resources/exception/StandardError.java
@@ -1,65 +1,22 @@
 package com.gmess.workshopmongodb.resources.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.io.Serial;
 import java.io.Serializable;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class StandardError implements Serializable {
     @Serial
     private static final long serialVersionUID = -4858016592584623132L;
     private Long timestamp;
     private Integer status;
-    private String error;
-    private String message;
-    private String path;
+    private String error, message, path;
 
-    public StandardError(){
-    }
-
-    public StandardError(Long timestamp, Integer status, String error, String message, String path) {
-        this.timestamp = timestamp;
-        this.status = status;
-        this.error = error;
-        this.message = message;
-        this.path = path;
-    }
-
-    public Long getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(Long timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public void setError(String error) {
-        this.error = error;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
 }
