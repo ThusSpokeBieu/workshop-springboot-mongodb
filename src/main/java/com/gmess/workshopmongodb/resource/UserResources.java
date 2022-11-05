dcda7a50322fdf30334002f66caf4a89a9c3fefb
Gabriel Messias Costa <gmessiasc@gmail.com>
2022-11-05 16:37:44 -0300

    endpoint to retorn user's posts

--- src/main/java/com/gmess/workshopmongodb/resource/UserResources.java
+++ src/main/java/com/gmess/workshopmongodb/resource/UserResources.java
@@ -1,5 +1,6 @@
 package com.gmess.workshopmongodb.resource;
 
+import com.gmess.workshopmongodb.domain.Post;
 import com.gmess.workshopmongodb.domain.User;
 import com.gmess.workshopmongodb.dto.UserDTO;
 import com.gmess.workshopmongodb.mapper.UserMapper;
@@ -56,5 +57,11 @@ public class UserResources {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value="/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
